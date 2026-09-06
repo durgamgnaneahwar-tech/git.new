4ae3a0697fc753020dc22301680fb013d51ea68e
durgamgnaneahwar-tech <durgamgnaneshwar2003@gmail.com>
2026-09-06 09:53:40 +0530

    odd

--- fun.py
+++ fun.py
@@ -266,3 +266,11 @@ def even(list):
             l.append(i)
     return l
 print(even([1,2,3,4,5,6,7,8,9,10]))
+
+def odd(list):
+    l=[]
+    for i in list:
+        if i%2!=0:
+            l.append(i)
+    return l
+print(odd([1,2,3,4,5,6,7,8,9,10]))
